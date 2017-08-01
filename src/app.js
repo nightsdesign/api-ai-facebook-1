1067c3d6da09f60a14e56ada868ca673dfc54be6
nightsdesign <nightsdesign@gmail.com>
2017-08-01 23:56:24 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const async = require('async');
 
 const REST_PORT = (process.env.PORT || 5000);
 const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
-const APIAI_LANG = process.env.APIAI_LANG || 'en';
+const APIAI_LANG = process.env.APIAI_LANG || 'zh-HK';
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
 const FB_TEXT_LIMIT = 640;
